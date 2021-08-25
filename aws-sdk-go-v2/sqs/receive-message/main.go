7e3689de307742e5020c8467264f2bf6c8402002
Gavinda Kinandana <gavindakinandana@gmail.com>
2021-08-25 11:38:03 +0700

    restructure sqs receive message

--- aws-sdk-go-v2/sqs/receive-message/main.go
+++ aws-sdk-go-v2/sqs/receive-message/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
@@ -12,32 +13,49 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
-func receiveSQSMessage() {
-	queue := "<QUEUE_NAME>"
-	timeout := 5
-	ctx := context.Background()
+type SQSLocalstack struct {
+	region         string
+	queueName      string
+	timeoutSeconds int
+	localstackURL  string
+}
 
-	if queue == "" {
-		fmt.Println("You must supply the name of a queue")
+// setup the data by collecting from args parameter
+func (s *SQSLocalstack) setup() {
+	region := flag.String("r", "us-east-1", "The region of localstack. Default: us-east-1")
+	queue := flag.String("q", "", "The name of the queue")
+	timeout := flag.Int("t", 5, "How long, in seconds, that the message is hidden from others")
+	localstackURL := flag.String("u", "http://localhost:4566", "The Localstack url. Default : http://localhost:4566")
+	flag.Parse()
+
+	if *queue == "" {
+		fmt.Println("You must supply the name of a queue (-q QUEUE)")
 		return
 	}
 
-	if timeout < 0 {
-		timeout = 0
+	if *timeout < 0 {
+		*timeout = 0
 	}
 
-	if timeout > 12*60*60 {
-		timeout = 12 * 60 * 60
+	if *timeout > 12*60*60 {
+		*timeout = 12 * 60 * 60
 	}
+	s.region = *region
+	s.queueName = *queue
+	s.timeoutSeconds = *timeout
+	s.localstackURL = *localstackURL
+}
 
-	endpointURL := "<LOCALSTACK_URL>"
-	region := "us-east-1"
+func (s *SQSLocalstack) receiveSQSMessage() error {
+	ctx := context.TODO()
+
+	// customResolver used to handle the localstack url we given to aws-sdk-go-v2
 	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
-		if endpointURL != "" {
+		if s.localstackURL != "" {
 			return aws.Endpoint{
 				PartitionID:   "aws",
-				URL:           endpointURL,
-				SigningRegion: region,
+				URL:           s.localstackURL,
+				SigningRegion: s.region,
 			}, nil
 		}
 		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
@@ -46,7 +64,7 @@ func receiveSQSMessage() {
 
 	cfg, err := config.LoadDefaultConfig(
 		ctx,
-		config.WithRegion(region),
+		config.WithRegion(s.region),
 		config.WithEndpointResolver(customResolver),
 	)
 	if err != nil {
@@ -56,14 +74,14 @@ func receiveSQSMessage() {
 	client := sqs.NewFromConfig(cfg)
 
 	gQInput := &sqs.GetQueueUrlInput{
-		QueueName: &queue,
+		QueueName: &s.queueName,
 	}
 
 	// Get URL of queue
 	urlResult, err := client.GetQueueUrl(ctx, gQInput)
 	if err != nil {
 		fmt.Println("Got an error getting the queue URL:", err)
-		return
+		return err
 	}
 
 	queueURL := urlResult.QueueUrl
@@ -74,20 +92,21 @@ func receiveSQSMessage() {
 		},
 		QueueUrl:            queueURL,
 		MaxNumberOfMessages: 1,
-		VisibilityTimeout:   int32(timeout),
+		VisibilityTimeout:   int32(s.timeoutSeconds),
 	}
 
 	msgResult, err := client.ReceiveMessage(ctx, gMInput)
 	if err != nil {
 		fmt.Println("Got an error receiving messages:")
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	if msgResult != nil {
 		messages := msgResult.Messages
 		if len(messages) > 0 {
 			msg := messages[0]
+			fmt.Println("Message received")
 			fmt.Println("Message ID:     " + *msg.MessageId)
 			fmt.Println("Message Body: " + *msg.Body)
 
@@ -99,14 +118,19 @@ func receiveSQSMessage() {
 			_, err := client.DeleteMessage(ctx, dMInput)
 			if err != nil {
 				fmt.Println("failed to delete message", err)
+				return err
 			}
-			fmt.Println("Message was deleted")
+			fmt.Println("Message succesfully deleted")
 		}
 	}
-
+	return nil
 }
 
 func main() {
+	sqsLocalstack := SQSLocalstack{}
+
+	sqsLocalstack.setup()
+
 	fmt.Println("Start listen incoming message")
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
@@ -116,7 +140,10 @@ func main() {
 			fmt.Println("app stopped")
 			os.Exit(0)
 		default:
-			receiveSQSMessage()
+			err := sqsLocalstack.receiveSQSMessage()
+			if err != nil {
+				os.Exit(1)
+			}
 		}
 	}
 }
